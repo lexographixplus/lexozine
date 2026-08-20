1ab3b686d92d82828bb74bf16b6327841665bd97
lexographixplus <lexographixplus@gmail.com>
2026-08-20 13:46:16 +0000

    style: load live edition refinements

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,5 +1,6 @@
 import DigitalEdition from "@/components/digital-edition";
 import "./preview.css";
+import "./preview-v2.css";
 
 export default function PreviewPage() {
   return <DigitalEdition />;
