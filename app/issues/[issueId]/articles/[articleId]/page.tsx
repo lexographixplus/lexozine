f7c3e857beda38ee1f97cb5c43060a98a42d9c99
lexographixplus <lexographixplus@gmail.com>
2026-08-21 23:46:12 +0000

    style: load release 0.7 editor UX

--- app/issues/[issueId]/articles/[articleId]/page.tsx
+++ app/issues/[issueId]/articles/[articleId]/page.tsx
@@ -1,6 +1,7 @@
 import ArticleEditor from "@/components/article-editor";
 import "../../../editorial-workflow.css";
 import "../../../block-editor-refinement.css";
+import "../../../release-07.css";
 
 export default async function ArticleEditorPage({ params }: { params: Promise<{ issueId: string; articleId: string }> }) {
   const { issueId, articleId } = await params;
