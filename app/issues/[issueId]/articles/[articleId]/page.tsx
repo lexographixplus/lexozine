efcb5b79dd001c4b6ce3ca5ccfe1871054fd5213
lexographixplus <lexographixplus@gmail.com>
2026-08-21 11:14:44 +0000

    style: load block editor refinement

--- app/issues/[issueId]/articles/[articleId]/page.tsx
+++ app/issues/[issueId]/articles/[articleId]/page.tsx
@@ -1,5 +1,6 @@
 import ArticleEditor from "@/components/article-editor";
 import "../../../editorial-workflow.css";
+import "../../../block-editor-refinement.css";
 
 export default async function ArticleEditorPage({ params }: { params: Promise<{ issueId: string; articleId: string }> }) {
   const { issueId, articleId } = await params;
