d537a03cdb34a67c7a859095ad1e7a3d6148371f
lexographixplus <lexographixplus@gmail.com>
2026-08-21 23:36:20 +0000

    style: load article creation refinement

--- app/issues/[issueId]/page.tsx
+++ app/issues/[issueId]/page.tsx
@@ -1,5 +1,6 @@
 import IssueWorkspace from "@/components/issue-workspace";
 import "../editorial-workflow.css";
+import "../creation-refinement.css";
 
 export default async function IssueWorkspacePage({ params }: { params: Promise<{ issueId: string }> }) {
   const { issueId } = await params;
