8fd843e87dc452985a7b68bbf73bf171a0fcbb1b
lexographixplus <lexographixplus@gmail.com>
2026-08-21 23:48:32 +0000

    feat: carry issue navigation into preview

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,12 +1,14 @@
 import DigitalEdition from "@/components/digital-edition";
+import IssueNavigation from "@/components/issue-navigation";
 import { getIssue, listIssues } from "@/lib/server/issue-repository";
 import "./preview.css";
 import "./preview-v2.css";
+import "../issues/release-07.css";
 
 export const dynamic = "force-dynamic";
 
 export default async function PreviewPage({ searchParams }: { searchParams: Promise<{ issue?: string }> }) {
   const { issue: issueId } = await searchParams;
   const issue = issueId ? await getIssue(issueId) : (await listIssues())[0] ?? null;
-  return <DigitalEdition initialIssue={issue ?? undefined} />;
+  return <>{issue ? <IssueNavigation issueId={issue.id} active="preview"/> : null}<DigitalEdition initialIssue={issue ?? undefined} /></>;
 }
