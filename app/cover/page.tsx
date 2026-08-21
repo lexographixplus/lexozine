1df6ccc586e0855914fdb22e4b34118cf9888d16
lexographixplus <lexographixplus@gmail.com>
2026-08-21 23:48:03 +0000

    feat: carry issue navigation into cover studio

--- app/cover/page.tsx
+++ app/cover/page.tsx
@@ -1,6 +1,9 @@
 import CoverWorkspace from "@/components/cover-workspace";
+import IssueNavigation from "@/components/issue-navigation";
 import "./cover.css";
+import "../issues/release-07.css";
 
-export default function CoverPage() {
-  return <CoverWorkspace />;
+export default async function CoverPage({ searchParams }: { searchParams: Promise<{ issue?: string }> }) {
+  const { issue } = await searchParams;
+  return <>{issue ? <IssueNavigation issueId={issue} active="cover"/> : null}<CoverWorkspace /></>;
 }
