eee7bf0372963b5cba4d0e2b00e45bf17649a23c
lexographixplus <lexographixplus@gmail.com>
2026-08-21 23:48:09 +0000

    feat: carry issue navigation into media library

--- app/media/page.tsx
+++ app/media/page.tsx
@@ -1,6 +1,9 @@
+import IssueNavigation from "@/components/issue-navigation";
 import MediaLibrary from "@/components/media-library";
 import "./media.css";
+import "../issues/release-07.css";
 
-export default function MediaPage() {
-  return <MediaLibrary />;
+export default async function MediaPage({ searchParams }: { searchParams: Promise<{ issue?: string }> }) {
+  const { issue } = await searchParams;
+  return <>{issue ? <IssueNavigation issueId={issue} active="media"/> : null}<MediaLibrary /></>;
 }
