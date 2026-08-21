bb297ec73785ca21b130d8a6f5c18d95b99bcf83
lexographixplus <lexographixplus@gmail.com>
2026-08-21 23:48:26 +0000

    feat: add issue navigation to review page

--- app/review/page.tsx
+++ app/review/page.tsx
@@ -1,5 +1,10 @@
+import IssueNavigation from "@/components/issue-navigation";
 import ReviewWorkspace from "@/components/review-workspace";
 import "../utility.css";
 import "./review.css";
+import "../issues/release-07.css";
 
-export default function ReviewPage(){return <ReviewWorkspace/>}
+export default async function ReviewPage({ searchParams }: { searchParams: Promise<{ issue?: string }> }) {
+  const params = await searchParams;
+  return <>{params.issue ? <IssueNavigation issueId={params.issue} active="review"/> : null}<ReviewWorkspace/></>;
+}
