896503deee96ff4080c25da7c6329f6e1a139669
lexographixplus <lexographixplus@gmail.com>
2026-08-22 00:12:41 +0000

    style: load editors note shortcut

--- app/issues/[issueId]/page.tsx
+++ app/issues/[issueId]/page.tsx
@@ -3,6 +3,7 @@ import IssueWorkspace from "@/components/issue-workspace";
 import "../editorial-workflow.css";
 import "../creation-refinement.css";
 import "../release-07.css";
+import "../editors-note.css";
 
 export default async function IssueWorkspacePage({ params }: { params: Promise<{ issueId: string }> }) {
   const { issueId } = await params;
